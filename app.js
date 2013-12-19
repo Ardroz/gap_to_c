fc09a15f1e71840a3570128453603590d6a9a9dd
luricardorod <luricardorod@gmail.com>
2013-12-18 20:31:23 -0600

    sfc step

--- app.js
+++ app.js
@@ -604,56 +604,41 @@ function blockSwitch ( parameters ) {
       break;
 
     case "SFC_STEP":
+      var tcond = new Array;
+      var outputinputs = new Array;
+
       parameters.fields.forEach( function ( element, index, array ) {
         if ( element.IOType === 'Input' || element.IOType === 'Tune' ) {
           if ( element.FieldName.substring(0,7) === 'SF_SEL_' ) {
             if( /[a-z]/i.test( element.Value ) ){
-              inputValues.push( {
-                field: element.FieldName,
-                value: element.Value.replace(/\56/g,"_")
-              } );
-            }else{
-              inputValues.push( {
-                field: element.FieldName,
-                value: element.Value
-              } );
+              inputValues.push( element.Value.replace(/\56/g,"_"));
+            } else {
+              inputValues.push( element.Value );
             }
+          } else {
+            tcond.push( element.Value.replace(/\56/g,"_") );
           }
+          
         } else if ( element.IOType === 'Output' ) {
-          outputValues.push(element.FieldName);
-          console.log( element.FieldName );
+          outputValues.push( parameters.block.Category + '_' + parameters.block.Name + '_' + element.FieldName);
+          outputinputs.push( parameters.block.Category + '_' + parameters.block.Name + '_' + element.FieldName);
+
         }
       });
+      tcond = tcond.slice(2,tcond.length);
 
-      inputValues.sort( function ( a, b ) {
-        if ( Number(a.field.substring( 7, a.field.length )) > 
-          Number(b.field.substring( 7, b.field.length )))
-          return 1;
-        if ( Number(a.field.substring( 7, a.field.length )) < 
-          Number(b.field.substring( 7, b.field.length )))
-          return -1;
-        return 0;
-      });
-      var switchCase = parameters.block.Category + '_' + parameters.block.Name + 
-        '_' + parameters.block.BlockType + '_COUNTER';
-
-      code = 'int ' + switchCase + ' = 1;\n';
-
-      code+= 'switch( ' + switchCase + ' ) {\n';
-      
-      inputValues.forEach( function ( element, index, array ) {
-        code+= '\tcase ' + (index + 1) + ':\n';
-        code+= '\t\tif ( ' + element.value + ' ) {\n';
-        code+= '\t\t\t' + switchCase + '++;\n';
-        code+= '\t\t}\n';
-        code+= '\t\tbreak;\n';
-
-        if ( (index + 1) === array.length ){
-          code+= switchCase.substring( 0, (switchCase.length - 8) ) + '';;
-        }
+      outputinputs = outputinputs.splice(2,outputValues.length);
+      stringInputs = inputValues.join( ' || ' );
+      code = 'if('  + stringInputs + ') { \n\t';
+      tcond.forEach( function ( element, index, array ) {
+        code += 'if( ' + element + ') {\n';
+        code += '\t\t' + outputinputs[index] + ' = ' + element + ';\n';
+        code += '\t} else ';
       });
 
-      code+= '}\n';
+      code += '\n\t\t' + outputValues[1] + '= 1\n';
+
+      code += '} else {\n  ' + outputValues.join('= 0; \n  ') + ' = 0; \n}\n';
 
       return code;
       break;
